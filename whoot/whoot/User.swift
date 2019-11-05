81d90c9a8432c33e6ac63cffcad3ddd6897fd709
Cvitale95 <38443481+Cvitale95@users.noreply.github.com>
2019-11-04 19:26:38 -0800

    post class
    Created post class for user and post

---
+++ whoot/whoot/User.swift
@@ -0,0 +1,16 @@
+//
+//  User.swift
+//  whoot
+//
+//  Created by Chris  on 11/4/19.
+//  Copyright © 2019 Carlos Estrada. All rights reserved.
+//
+
+import Foundation
+
+class User{
+    var UID: String = ""
+    var upVotes: Int = 0
+    var downVotes = 0
+    var Posts: [Int] = [] 
+}
