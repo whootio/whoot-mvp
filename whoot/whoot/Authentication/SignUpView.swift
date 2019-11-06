2474761e87f1e9c20e9a6779ca3602bd76b9f2d8
Carlos Estrada <me@caestrada.com>
2019-11-05 23:01:33 -0800

    Use DBHelper to sign up users

--- whoot/whoot/Authentication/SignUpView.swift
+++ whoot/whoot/Authentication/SignUpView.swift
@@ -24,38 +24,26 @@ class SignUpView: UIViewController {
         guard let password = passwordField.text else { return }
         guard let email = emailField.text else  { return }
         
-        Auth.auth().createUser(withEmail: email, password: password) { result, error in
+        DBHelper.createUser(email: email, password: password) { result, error in
             if let error = error {
                 
                 // Something went wrong and we were not able to create the user's account
-                
+                // Show the user an error alert
+
                 let alert = UIAlertController(title: "Sign Up Error",
                       message: error.localizedDescription, preferredStyle: .alert)
                 alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
                 self.present(alert, animated: true)
                 
-            } else {
+            }
+            else {
+                // Successfully created the user and added them to the database
+                // Continue to home feed
                 
-                // Add user to database
-                
-                guard let uid = result?.user.uid else { return }
-                
-                let values = ["email": email]
-                
-                Database.database().reference().child("users").child(uid).updateChildValues(values, withCompletionBlock: { (error, ref) in
-                    if let error = error {
-                        print("failed to update database values \(error.localizedDescription)")
-                        return
-                    }
-                })
-                
-                // Successfully created the user
                 print("user create success")
-                
                 self.performSegue(withIdentifier: "createAccountSegue", sender: nil)
                 
             }
-            
         }
     }
 }
