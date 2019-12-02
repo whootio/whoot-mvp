8f776d963743985f9b6ecf77d80ca38a4db3ce1b
Carlos Estrada <me@caestrada.com>
2019-12-02 15:07:26 -0800

    Add a settings page under the profile tab

---
+++ whoot/whoot/Profile/SettingsViewController.swift
@@ -0,0 +1,114 @@
+//
+//  SettingsViewController.swift
+//  whoot
+//
+//  Created by Carlos Estrada on 12/2/19.
+//  Copyright © 2019 Carlos Estrada. All rights reserved.
+//
+
+import UIKit
+import FirebaseAuth
+
+class SettingsViewController: UITableViewController {
+
+    // Location Settings
+    @IBOutlet weak var locationUnits: UISegmentedControl!
+    @IBOutlet weak var locationDistance: UILabel!
+    
+    // Profile Settings
+    @IBOutlet weak var publicProfile: UISwitch!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    // MARK: - Sign Out
+    func signOut(_ sender: Any) {
+        do {
+            try Auth.auth().signOut()
+            let mainStoryboard:UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+            let signIn = mainStoryboard.instantiateViewController(withIdentifier: "SignInView") as! SignInView
+            self.present(signIn, animated: true, completion: nil)
+            print("Sign out success")
+        } catch let error as NSError {
+            let alert = UIAlertController(title: "Error signing out",
+                  message: error.localizedDescription, preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alert, animated: true)
+        }
+    }
+    
+    // MARK: - Table view data source
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath.section == 2 && indexPath.row == 0 {
+            signOut(self)
+            self.tableView.deselectRow(at: indexPath, animated: true)
+    }
+
+    }
+    
+    /*
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
